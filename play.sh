2d4e3e9189e1bc8d4fad470c9ebb4ba6e2ef9b50
Yihan Song <yihan.song@sap.com>
2022-12-16 16:29:25 +0800

    use mpd as a playing backend

--- play.sh
+++ play.sh
@@ -14,22 +14,25 @@ PLAYLISTS=
 if [ ! -f cookie.txt ]; then
   curl -s -c cookie.txt "${NETEASE_MUSIC_API}/login?email=${NETEASE_MUSIC_USERNAME}&password=${NETEASE_MUSIC_PASSWORD}" > .login.json
 
-
   if [ `jq -r '.code' .login.json` -ne 200 ]; then
     echo "login failed"
     exit 1
   fi
 fi
 
-play() {
+log() {
+  logger $1
+  echo $1
+}
 
+play() {
   if [[ -f .cache/$1.mp3 ]]; then
-    echo "found $1 in cache"
+    log "found $1 in cache"
 
     albumid=`jq -r '.songs[0].al.id' .cache/$1.detail.json`
     name=`jq -r '.songs[0].name' .cache/$1.detail.json`
   else
-    echo "check whether $1 is available"
+    log "check whether $1 is available"
     curl -s "${NETEASE_MUSIC_API}/check/music?id=$1" > .cache/$1.check.json
 
     jq -r '.message' .cache/$1.check.json
@@ -48,20 +51,20 @@ play() {
 
     url=`jq -r '.data[0].url' .cache/$1.url.json`
     if [[ "$url" == 'null' ]]; then
-      echo "cannot extract url for $name"
+      log "cannot extract url for $name"
       rm .cache/$1.url.json .cache/$1.detail.json
       return -1
     fi
 
-    echo "downloading $name"
+    log "downloading $name"
     curl -s $url -o .cache/$1.mp3
   fi
 
-  echo "playing $name"
-  mpg321 -x .cache/$1.mp3
+  log "playing $name"
+  mpc add .cache/$1.mp3
 
-  echo "scrobbling $name"
-  curl -s -b cookie.txt "${NETEASE_MUSIC_API}/scrobble?id=$1&sourceid=$albumid"
+  #log "scrobbling $name"
+  #curl -s -b cookie.txt "${NETEASE_MUSIC_API}/scrobble?id=$1&sourceid=$albumid"
 
   return 0
 }
@@ -75,10 +78,12 @@ favorite() {
 }
 
 album() {
+  mpc clear
+  
   if [[ ! -f .cache/$1.album.json ]]; then
     curl -s "${NETEASE_MUSIC_API}/album?id=$1" > .cache/$1.album.json
   fi
-  
+
   jq -r '.songs | sort_by(.no)[] | .id' .cache/$1.album.json | while read id; do
     play $id
   done
@@ -86,7 +91,8 @@ album() {
 }
 
 if [[ `jq -r "has(\"$1\")" meta.json` -ne 'true' ]]; then
-  echo "Cannot find metadata for this NFC tag $1"
+  log "Cannot find metadata for this NFC tag $1， register it temporarily"
+  jq ".\"$1\" = { type }" meta.json
   exit -1
 fi
 
--- stop.sh
+++ stop.sh
@@ -3,6 +3,7 @@ cd "$(dirname "$0")"
 
 kill -9 `cat .ppid`
 kill -9 `cat .pid`
-pkill mpg321
-rm -f .*.json .*.mp3 .pid .ppid
+mpc stop
+mpc clear
+rm -f .*.json .pid .ppid
 
